17348a284e095d81b1fc6a45122e340f046239d2
Jacob B. Cholewa <jbec@itu.dk>
2016-08-17 11:00:04 +0200

    Some changes for Vehicle and added DMR contract

---
+++ src/contracts/DMR.sol
@@ -0,0 +1,5 @@
+import "Vehicle.sol";
+
+contract DMR {
+	
+}
--- src/contracts/Vehicle.sol
+++ src/contracts/Vehicle.sol
@@ -1,24 +1,33 @@
 contract Vehicle {
-  address private owner;
-  mapping (uint => address) owner_history;
-  uint num_previous_owners;
+	/* Fields */
+	address private owner;
+	uint public numPreviousOwners = 0;
+	uint public serialNumber;
 
-  function Vehicle () {
-    owner = msg.sender;
-  }
+	/* Events */
+	event OwnershipChanged(address newOwner);
 
-  modifier isOwner () {
-    if (msg.sender != owner) {
-      throw;
-    }
-  }
-  
-  function transfer() {
-    
-  }
-  
-  function () {
-    throw;
-  }
+	/* Mappings */
+	mapping (uint => address) OwnerHistory;
 
+	function Vehicle (uint _serialNumber) {
+		owner = msg.sender;
+		serialNumber = _serialNumber;
+	}
+
+	modifier isOwner () {
+		if (msg.sender != owner) {
+			throw;
+		}
+	}
+
+	function transfer(address newOwner) isOwner {
+		OwnerHistory[numPreviousOwners++] = owner;
+		owner = newOwner;
+		OwnershipChanged(newOwner);
+	}
+
+	function () {
+		throw;
+	}
 }
