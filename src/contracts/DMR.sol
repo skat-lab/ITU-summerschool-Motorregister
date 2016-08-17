1aff8c99bbb802d69b13d783523e647922eac448
Jacob B. Cholewa <jbec@itu.dk>
2016-08-17 16:11:20 +0200

    Forgot to add...

--- src/contracts/DMR.sol
+++
@@ -1,5 +0,0 @@
-import "Vehicle.sol";
-
-contract DMR {
-	
-}
--- src/contracts/Tradeable.sol
+++ src/contracts/Tradeable.sol
@@ -1,23 +1,31 @@
+import "Marketplace.sol";
+
 contract Tradeable {
 
 	/* Fields */
-	address owner;
+	address public owner;
 	address market;
-	address buyer;
 
 	/* Modifiers */
 	modifier isOwner() { if(msg.sender != owner) throw; }
 	modifier isMarket() { if(msg.sender != market) throw; }
 
-	function allowTransfer(address _market, address _to) isOwner {
+	function sell(Marketplace _market, address _buyer, uint _amount) isOwner {
+		_market.makeOffer(this, owner, _buyer, _amount);
 		market = _market;
-		buyer = _to;
 	}
 
-	function transfer(address to) isMarket {
-		owner = buyer;
+	function transferContract(address to) isMarket {
+		owner = to;
 		market = 0;
-		buyer = 0;
+	}
+
+	function cancelSale() isMarket {
+		market = 0;
+	}
+
+	function () {
+		throw;
 	}
 }
 
